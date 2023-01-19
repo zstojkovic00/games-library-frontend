effffe2315d742c5357e7e684f1f7cbc647421f7
zstojkovic00 <00zeljkostojkovic@gmail.com>
2023-01-19 02:12:41 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar/navbar";
+import Home from"./pages/Home/home"
+import Login from "./pages/Login/login"
+import Join from "./pages/Join/join"
+import { Route, Routes} from "react-router-dom";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+        <div className="container">
+           <Routes >
+               <Route path="/" element={<Home/>} />
+               <Route path="/login" element={<Login/>} />
+               <Route path="/join" element={<Join/>} />
+
+           </Routes>
+        </div>
     </div>
   );
 }
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./navbar.css";
+import Logo from '../../assets/images/logo.png'
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+     <nav className="nav">
+         <Link to="/" className="site-logo"> <img src={Logo} alt="Logo"/></Link>
+         <ul>
+             <li className="nav__el--login">
+                 <Link className="nav__el--login-top" to="/login"> Login</Link>
+             </li>
+
+             <li className="nav__el--login">
+                 <Link  className="nav__el--login-top" to="/join"> Join</Link>
+             </li >
+
+
+
+         </ul>
+     </nav>
+    );
+};
+
+export default Navbar;
