e8eb61ef6e8743d05a042c66905412040b11f810
zstojkovic00 <00zeljkostojkovic@gmail.com>
2023-02-18 14:06:05 +0100

    Game Display page UI

--- src/pages/GameDisplay.jsx
+++ src/pages/GameDisplay.jsx
@@ -3,31 +3,22 @@ import axios from "axios";
 import {useNavigate, useParams} from "react-router-dom";
 import starFull from '../assets/images/star-full.png';
 import starEmpty from '../assets/images/star-empty.png';
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import './style/gamedisplay.css'
-import { smallImage } from "../util/util";
-import apple from '../assets/images/apple.svg'
-import gamepad from '../assets/images/gamepad.svg'
-import nintendo from'../assets/images/nintendo.svg'
-import playstation from'../assets/images/playstation.svg'
-import steam from '../assets/images/steam.svg'
-import xbox from '../assets/images/xbox.svg'
+import {smallImage} from "../util/util";
 import {addGameToCurrentUser} from "../api/authenticationService";
-
-
-
-
 
 
 const GameDisplay = ({...props}) => {
 
     const navigate = useNavigate();
-
     const [game, setGame] = useState([]);
-
+    const [gamePhoto, setGamePhoto] = useState([]);
     const {id} = useParams();
-
-
+    const [currentImage, setCurrentImage] = useState(game.background_image);
+    const changeImage = (data) => {
+        setCurrentImage(data.image);
+    };
 
     const getStars = () => {
         const stars = [];
@@ -42,60 +33,39 @@ const GameDisplay = ({...props}) => {
         return stars;
     };
 
-    const getPlatform = (platform) => {
 
-        switch (platform) {
-            case "PlayStation 4":
-                return playstation;
-            case "PlayStation 5":
-                return playstation;
-            case "Xbox One":
-                return xbox;
-            case "PC":
-                return steam;
-            case "Nintendo Switch":
-                return nintendo;
-            case "iOS":
-                return apple;
-            default:
-                return gamepad;
-        }
-
-
-
-    };
-
-
-    const getGame = async() => {
-        const {data: result} = await axios.get(`https://api.rawg.io/api/games/${id}?key=`+process.env.REACT_APP_API_KEY,{
-        })
+    const getGame = async () => {
+        const {data: result} = await axios.get(`https://api.rawg.io/api/games/${id}?key=` + process.env.REACT_APP_API_KEY, {})
         setGame(result);
         console.log(result);
     }
 
-    useEffect(()=> {
+    const GetGameScreenshots = async () => {
+        const {data: screenshots} = await axios.get(`https://rawg.io/api/games/${id}/screenshots?key=` + process.env.REACT_APP_API_KEY, {})
+        setGamePhoto(screenshots);
+        console.log(screenshots);
+
+    }
+
+    useEffect(() => {
         getGame();
+        GetGameScreenshots();
 
     }, []);
-
-
-
-
-
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        addGameToCurrentUser(id).then((res)=>{
+        addGameToCurrentUser(id).then((res) => {
 
-            console.log("response",res);
-            if(res.status===200){
+            console.log("response", res);
+            if (res.status === 200) {
                 navigate("/my-games");
             }
 
 
-        }).catch((err)=>{
+        }).catch((err) => {
 
             console.log(err);
 
@@ -105,44 +75,52 @@ const GameDisplay = ({...props}) => {
 
 
     return (
-        <div >
-            <div className="gameDisplay__wrapper" >
-            <div className="rating__wrapper">
-                <h1>{game.name}</h1>
-                <p>Rating: {game.rating}</p>
-                {getStars()}
-            </div>
-            <div className="platforms__wrapper">
-            <h1>Platforms</h1>
-                {game.platforms?.map((data) => (
-                    <img
-                        alt={data.platform.name}
-                        key={data.platform.id}
-                        src={ getPlatform(data.platform.name)}
-                    ></img>
-                ))}
-            </div>
 
-                <div className='image__wrapper' >
+        <div className="container flex">
+            <div className="left">
+                <div className="main_image">
                     <motion.img
                         src={smallImage(game.background_image, 1280)}
                         alt={game.background_image}
                     />
+                    <div className="option flex">
+                        {gamePhoto.results?.map((data) => (
+                            <img
+                                alt={data.id}
+                                key={data.id}
+                                src={data.image}
+                            ></img>
+                        ))}
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="right">
+                <h3 className="gameName">{game.name}</h3>
+                <p className="gameReleased">Released: {new Date(game.released).toLocaleDateString()}</p>
+                <h4 className="gameRating"> Rating: {game.rating} {getStars()}  </h4>
+                <p className="gameDescription">
+                    {game.description_raw}
+                </p>
+                <div class="gameInfo">
+                    <p className="gamePlatforms">Platforms: {game.platforms?.map((p) => p.platform.name).join(', ')}</p>
+                    <p className="gameGenres">Genres: {game.genres?.map((g) => g.name).join(', ')}</p>
+                    <p className="gamePublishers">Publishers: {game.publishers?.map((p) => p.name).join(', ')}</p>
                 </div>
 
-                <form onSubmit={handleSubmit}>
-                    <button type="submit">Add game</button>
+                <form className="game_container" onSubmit={handleSubmit}>
+                    <div className="search__button">
+                        <button className="search__button-top"> Add Game</button>
+                    </div>
                 </form>
             </div>
+
         </div>
 
 
     );
 };
-
-
-
-
 
 
 export default GameDisplay
